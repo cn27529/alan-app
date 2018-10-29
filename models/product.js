09e32e5c413e39daef72158e6114c5b988ad9206
Bruce Huang 黃耀進 <12061117@cecdm.com.tw>
2018-10-29 16:34:51 +0800

    Dockerfile, .dockerignore, docker-compose.debug.yml, docker-compose.yml

--- models/product.js
+++ models/product.js
@@ -11,20 +11,24 @@ module.exports = function (sequelize, DataTypes) {
         PUnit: DataTypes.STRING, //
         localtime: DataTypes.STRING //
     }, {
-        freezeTableName: false,
-        classMethods: {
-          associate: function(models) {
-            // Product.belongsTo(models.Classification, {
-            //   onDelete: "CASCADE",
-            //   foreignKey: {
-            //     allowNull: false
-            //   }
-            // });
+            freezeTableName: false,
+            classMethods: {
+                associate: function (models) {
+                    // Product.belongsTo(models.Classification, {
+                    //   onDelete: "CASCADE",
+                    //   foreignKey: {
+                    //     allowNull: false
+                    //   }
+                    // });
 
-            Product.belongsTo(models.Classification, {foreignKey: 'PClass', targetKey: 'CId'});
+                    Product.belongsTo(models.Classification,
+                        {
+                            foreignKey: 'PClass',
+                            targetKey: 'CId'
+                        });
 
-          }
-        }
+                }
+            }
         });
 
     return Product;
