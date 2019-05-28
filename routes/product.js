6bc49797b68d6674d56e61b8a6d188b5b1abacc5
Bruce Huang 黃耀進 <12061117@cecdm.com.tw>
2019-05-28 17:57:44 +0800

    app.use(router_init); //on init router

--- routes/product.js
+++ routes/product.js
@@ -5,11 +5,8 @@ var fs = require('fs');
 var uuid = require('uuid/v4');
 var path = require('path');
 
-var logpath = './routes/product';
-var logger = require('../logConfig').logger(logpath, 'debug');
-
 var XLSX = require('xlsx');
-var json_settings = require("../data/settings.json");
+var json_settings = require('../data/settings.json');
 var a2j = require('../arraydata2json');
 var filepath = require('../filepath');
 
@@ -19,220 +16,202 @@ var _FAFWork = new FAFWork();
 var _err = require('../data/err');
 var models = require('../models');
 
+var logpath = './routes/product';
+var logger = require('../logConfig').logger(logpath, 'debug');
+
 //文件 https://cn27529.gitbooks.io/alan-app-api/content/product.html
 
-router.get('/', function (req, res) {
+router.get('/', function(req, res) {
+  var title = req.originalUrl + '';
+  logger.info(title);
 
-    var title = req.originalUrl + '';
-    logger.info(title);
+  var myFolder = './xlsx-import-files';
+  var folderFiles = _FAFWork.getFolderFiles(myFolder);
+  var returnFiles = _FAFWork.getReturnFiles(folderFiles, myFolder);
+  console.log(returnFiles);
 
-    var myFolder = './xlsx-import-files';
-    var folderFiles = _FAFWork.getFolderFiles(myFolder);
-    var returnFiles = _FAFWork.getReturnFiles(folderFiles, myFolder);
-    console.log(returnFiles);
+  res.render('product', {
+    title: title,
+    cool: cool(),
+    data: returnFiles,
+    layout: '_tocas-layout' //指定layout名可不需副名.ejs
+  });
+});
 
-    res.render('product', {
+router.get('/all', function(req, res) {
+  logger.info('/all');
+  var title = req.originalUrl + '';
+
+  var keyword = req.params.keyword;
+  //var token = req.params.token; //先不檢查
+  var json = {
+    msg: _err.UN1.VAL,
+    code: _err.UN1.KEY,
+    data: []
+  };
+
+  models.Product.findAll({
+    // where: {
+    //     tag: 'admin'
+    // },
+    //tableHint: TableHints.NOLOCK,
+    order: [
+      // Will escape username and validate DESC against a list of valid direction parameters
+      ['id', 'ASC']
+    ]
+  })
+    .then(function(data) {
+      //if (keyword != "Q_QtaiwanQvQ") data = cool(); console.log(data);
+      json.data = data;
+      json.code = _err.ALL.KEY;
+      json.msg = _err.ALL.VAL;
+      res.json(json);
+    })
+    .catch(function(err) {
+      console.log(err);
+      json.code = _err.UNSQL.KEY;
+      json.msg = err;
+      logger.error(err);
+      res.json(json);
+    });
+});
+
+router.get('/list', function(req, res) {
+  logger.info('/all');
+
+  var keyword = req.params.keyword;
+  //var token = req.params.token; //先不檢查
+  var json = {
+    msg: _err.UN1.VAL,
+    code: _err.UN1.KEY,
+    data: []
+  };
+
+  //var title = '所有產品清單';
+  var title = req.originalUrl + '';
+  var colnames = ['產品', '分類', '重', '單位'];
+
+  models.Product.findAll({
+    // where: {
+    //     tag: 'admin'
+    // },
+    //tableHint: TableHints.NOLOCK,
+    include: [
+      {
+        model: models.Classification
+        // where: {
+        //     year_birth: 1984
+        // }
+      }
+    ],
+    order: [
+      // Will escape username and validate DESC against a list of valid direction parameters
+      ['id', 'ASC']
+    ]
+  })
+    .then(function(data) {
+      //if (keyword != "Q_QtaiwanQvQ") data = cool(); console.log(data);
+      json.data = data;
+      json.code = _err.ALL.KEY;
+      json.msg = _err.ALL.VAL;
+      //res.json(json);
+
+      res.render('product-list', {
         title: title,
         cool: cool(),
-        data: returnFiles,
-        layout: '_tocas-layout' //指定layout名可不需副名.ejs 
+        data: json.data,
+        stringJson: JSON.stringify(json.data),
+        cols: colnames,
+        layout: '_tocas-layout' //指定layout名可不需副名.ejs
+      });
+    })
+    .catch(function(err) {
+      console.log(err);
+      json.code = _err.UNSQL.KEY;
+      json.msg = err;
+      logger.error(err);
+      //res.json(json);
     });
-
-});
-
-router.get('/all', function (req, res) {
-
-    logger.info('/all');
-    var title = req.originalUrl + '';
-
-    var keyword = req.params.keyword;
-    //var token = req.params.token; //先不檢查
-    var json = {
-        msg: _err.UN1.VAL,
-        code: _err.UN1.KEY,
-        data: []
-    };
-
-    models.Product.findAll({
-            // where: {
-            //     tag: 'admin'
-            // },
-            //tableHint: TableHints.NOLOCK,
-            order: [
-                // Will escape username and validate DESC against a list of valid direction parameters
-                ['id', 'ASC']
-            ]
-        }).then(function (data) {
-
-            //if (keyword != "Q_QtaiwanQvQ") data = cool(); console.log(data);
-            json.data = data;
-            json.code = _err.ALL.KEY
-            json.msg = _err.ALL.VAL
-            res.json(json);
-
-        })
-        .catch(function (err) {
-
-            console.log(err);
-            json.code = _err.UNSQL.KEY;
-            json.msg = err;
-            logger.error(err);
-            res.json(json);
-
-        });
-
-});
-
-router.get('/list', function (req, res) {
-
-    logger.info('/all');
-
-    var keyword = req.params.keyword;
-    //var token = req.params.token; //先不檢查
-    var json = {
-        msg: _err.UN1.VAL,
-        code: _err.UN1.KEY,
-        data: []
-    };
-
-    //var title = '所有產品清單';
-    var title = req.originalUrl + '';
-    var colnames = ['產品', '分類',  '重', '單位'];
-
-    models.Product.findAll({
-            // where: {
-            //     tag: 'admin'
-            // },
-            //tableHint: TableHints.NOLOCK,
-            include: [{
-                model: models.Classification,
-                // where: {
-                //     year_birth: 1984
-                // }
-            }],
-            order: [
-                // Will escape username and validate DESC against a list of valid direction parameters
-                ['id', 'ASC']
-            ]
-        }).then(function (data) {
-
-            //if (keyword != "Q_QtaiwanQvQ") data = cool(); console.log(data);
-            json.data = data;
-            json.code = _err.ALL.KEY;
-            json.msg = _err.ALL.VAL;
-            //res.json(json);
-
-            res.render('product-list', {
-                title: title,
-                cool: cool(),
-                data: json.data,
-                stringJson: JSON.stringify(json.data),
-                cols: colnames,
-                layout: '_tocas-layout' //指定layout名可不需副名.ejs 
-            });
-
-        })
-        .catch(function (err) {
-
-            console.log(err);
-            json.code = _err.UNSQL.KEY;
-            json.msg = err;
-            logger.error(err);
-            //res.json(json);
-
-        });
-
-
-
 });
 
 router.get('/bycategory/:v', function (req, res) {
+    //
+  logger.info('/by');
+  var value = req.params.v;
 
-    logger.info('/by');
-    var value = req.params.v;
+  console.log('value=', value);
 
-    console.log('value=', value);
+  var keyword = req.params.keyword;
+  //var token = req.params.token; //先不檢查
+  var json = {
+    msg: _err.UN1.VAL,
+    code: _err.UN1.KEY,
+    data: []
+  };
 
-    var keyword = req.params.keyword;
-    //var token = req.params.token; //先不檢查
-    var json = {
-        msg: _err.UN1.VAL,
-        code: _err.UN1.KEY,
-        data: []
-    };
+  //var title = '所有'+value+'產品';
+  var title = req.originalUrl + '';
 
-    //var title = '所有'+value+'產品';
-    var title = req.originalUrl + '';
-    if (value !== '') title = '所有'+value+'產品';
-    var colnames = ['產品', '分類',  '重', '單位'];
+  if (value !== '') title = '所有' + value + '產品';
+  var colnames = ['產品', '分類', '重', '單位'];
 
-    models.Product.findAll({
-            where: {
-                PClass: value
-            },
-            //tableHint: TableHints.NOLOCK,
-            include: [{
-                model: models.Classification,
-                // where: {
-                //     year_birth: 1984
-                // }
-            }],
-            order: [
-                // Will escape username and validate DESC against a list of valid direction parameters
-                ['id', 'ASC']
-            ]
-        }).then(function (data) {
+  models.Product.findAll({
+    where: {
+      PClass: value
+    },
+    //tableHint: TableHints.NOLOCK,
+    include: [
+      {
+        model: models.Classification
+        // where: {
+        //     year_birth: 1984
+        // }
+      }
+    ],
+    order: [
+      // Will escape username and validate DESC against a list of valid direction parameters
+      ['id', 'ASC']
+    ]
+  })
+    .then(function(data) {
+      //if (keyword != "Q_QtaiwanQvQ") data = cool(); console.log(data);
+      json.data = data;
+      json.code = _err.ALL.KEY;
+      json.msg = _err.ALL.VAL;
+      //res.json(json);
 
-            //if (keyword != "Q_QtaiwanQvQ") data = cool(); console.log(data);
-            json.data = data;
-            json.code = _err.ALL.KEY;
-            json.msg = _err.ALL.VAL;
-            //res.json(json);
-
-            res.render('product-list', {
-                title: title,
-                cool: cool(),
-                data: json.data,
-                stringJson: JSON.stringify(json.data),
-                cols: colnames,
-                layout: '_tocas-layout' //指定layout名可不需副名.ejs 
-            });
-
-        })
-        .catch(function (err) {
-
-            console.log(err);
-            json.code = _err.UNSQL.KEY;
-            json.msg = err;
-            logger.error(err);
-            //res.json(json);
-
-        });
-
-
-
-});
-
-router.get('/info/:filename', function (req, res) {
-
-    //var title = '/:filename running now.';
-    var title = req.originalUrl + '';
-    logger.info(title);
-
-    var filename = req.param.filename;
-    var content = '';
-
-    res.render('vendorinfo', {
+      res.render('product-list', {
         title: title,
         cool: cool(),
-        data: content,
-        layout: "_tocas-layout"
+        data: json.data,
+        stringJson: JSON.stringify(json.data),
+        cols: colnames,
+        layout: '_tocas-layout' //指定layout名可不需副名.ejs
+      });
+    })
+    .catch(function(err) {
+      console.log(err);
+      json.code = _err.UNSQL.KEY;
+      json.msg = err;
+      logger.error(err);
+      //res.json(json);
     });
-
 });
 
+router.get('/info/:filename', function(req, res) {
+  //var title = '/:filename running now.';
+  var title = req.originalUrl + '';
+  logger.info(title);
 
+  var filename = req.param.filename;
+  var content = '';
 
-
+  res.render('vendorinfo', {
+    title: title,
+    cool: cool(),
+    data: content,
+    layout: '_tocas-layout'
+  });
+});
 
 module.exports = router;
