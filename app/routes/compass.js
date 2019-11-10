7a5a8791e188ea639b259b07c11d5010d955e097
cn27529 <cn27529@hotmail.com>
2019-11-10 14:51:26 +0800

    compass1

--- app/routes/compass.js
+++ app/routes/compass.js
@@ -7,16 +7,16 @@ var logpath = './routes/compass';
 var logger = require('../logConfig').logger(logpath, 'debug');
 
 router.get('/', function(req, res, next) {
-  //call router_me middleware
-  var title = req.originalUrl + '';
+    //call router_me middleware
+    var title = req.originalUrl + '';
 
-  res.render('compass', {
-    title: title,
-    cool: cool(),
-    dataString: '',
-    layout: '_tocas-layout'
-  });
-  //end
+    res.render('compass1', {
+        title: title,
+        cool: cool(),
+        dataString: '',
+        layout: '_tocas-layout'
+    });
+    //end
 });
 
 module.exports = router;
